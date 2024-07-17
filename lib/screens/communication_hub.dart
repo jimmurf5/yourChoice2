d9844f6f7b2a2dd59a314505781c6640da03a042
James Murphy <jmurphy98@qub.ac.uk>
2024-07-17 22:43:36 +0100

    continuing to build out the messageHub screen

--- lib/screens/communication_hub.dart
+++ lib/screens/communication_hub.dart
@@ -1,10 +1,13 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:your_choice/models/message_card.dart';
 import 'package:your_choice/widgets/message_card_item.dart';
 
 class CommunicationHub extends StatefulWidget {
-  const CommunicationHub({super.key});
+  final String profileId;
+
+  const CommunicationHub({super.key, required this.profileId});
 
   @override
   State<CommunicationHub> createState() {
@@ -13,6 +16,8 @@ class CommunicationHub extends StatefulWidget {
 }
 
 class _CommunicationHubState extends State<CommunicationHub> {
+  //default the selected category to category 3
+  int selectedCategory = 3;
   //a list to hold the cards that are selected by a profile user's clicks
   List<MessageCard> selectedCards = [];
 
@@ -61,8 +66,52 @@ class _CommunicationHubState extends State<CommunicationHub> {
                 icon: const Icon(FontAwesomeIcons.trashCan),
               ),
             ],
-          )
+          ),
           //vertically scrolling column with message cards
+          Expanded(
+              child: StreamBuilder<QuerySnapshot>(
+                //get the message Card data from firebase only for our selected profile matching the chosen category
+                stream: FirebaseFirestore.instance
+                .collection('profiles')
+                .doc(widget.profileId)
+                    .collection('message_cards')
+                    .where('categoryId', isEqualTo: selectedCategory)
+                    .snapshots(),
+                builder: (context, snapshot) {
+                  if(!snapshot.hasData){
+                    return const Center(child: CircularProgressIndicator());
+                  }
+                  final cardDeck = snapshot.data!.docs.map(
+                      (doc){
+                        return MessageCard.fromMap(doc.data() as Map<String, dynamic>);
+                      }
+                  ).toList();
+                  return GridView.builder(
+                      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: 3,
+                        mainAxisExtent: 4.0,
+                        crossAxisSpacing: 4.0,
+                        childAspectRatio: 0.75,
+                      ),
+                      itemCount: cardDeck.length,
+                      itemBuilder: (context, index) {
+                        final card = cardDeck[index];
+                        return GestureDetector(
+                          onTap: () {
+                            setState(() {
+                              if (selectedCards.length < 3){
+                                selectedCards.add(card);
+                              }
+                            });
+                          },
+                          child: MessageCardItem(messageCard: card),
+                        );
+                      }
+                  );
+                },
+              )
+          ),
+          //horizontally scrolling row for the categories
         ],
       ),
     );
