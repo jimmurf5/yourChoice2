4a3a2567a2372c89f238bd898a31fc5954341f8e
James Murphy <jmurphy98@qub.ac.uk>
2024-08-04 15:08:38 +0100

    only added comments to profile_fs_service

--- lib/services/profile_firestore_service.dart
+++ lib/services/profile_firestore_service.dart
@@ -1,10 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
+/// A service class that handles Firestore operations related to profile colours,
+/// specifically for getting and updating profile colors.
 class ProfileFirestoreService {
   final FirebaseFirestore _db = FirebaseFirestore.instance;
 
-  // Method to get the profile colour from Firestore
+  /// Fetches the profile colour from Firestore for the given profile ID.
+  ///
+  /// This method retrieves the document for the specified profile, checks if
+  /// the colour field exists, and returns it. If the colour field does not exist,
+  /// a default colour is returned.
+  ///
+  /// [profileId] - The ID of the profile to fetch the color for.
+  ///
+  /// Returns a [Color] object representing the profile color.
   Future<Color> getProfileColor(String profileId) async {
     // Fetch the document for the profile
     DocumentSnapshot doc = await _db.collection('profiles').doc(profileId).get();
@@ -12,18 +22,28 @@ class ProfileFirestoreService {
     if (doc.exists && doc.data() != null) {
       Map<String, dynamic> data = doc.data() as Map<String, dynamic>;
       if (data.containsKey('colour')) {
+
         // Return the colour if it exists
         return Color(data['colour']);
       }
     }
     // Return a default colour if it does not exist
-    //ensuring null is not returned
+    // ensuring null is not returned
     return Colors.blue; // default colour
   }
 
-  // Method to update the profile colour in Firestore
+  /// Updates the profile colour in Firestore for the given profile ID.
+  ///
+  /// This method sets the new colour value in the document
+  /// for the specified profile.
+  ///
+  /// [profileId] - The ID of the profile to update the colour for.
+  /// [color] - The new [Color] object to set as the profile colour.
   Future<void> updateProfileColour(String profileId, Color color) async {
     // Update the document with the new colour value
-    await _db.collection('profiles').doc(profileId).set({'colour': color.value}, SetOptions(merge: true));
+    await _db
+        .collection('profiles')
+        .doc(profileId)
+        .set({'colour': color.value}, SetOptions(merge: true));
   }
 }
